0f46919e8178a9600e51f5e6457ec4caa8700907
foggybrains <dustin.keyes@gmail.com>
2019-06-13 18:11:05 -0600

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,39 @@
+require 'pry'
+
 class BankAccount
+    attr_reader :name
+    attr_accessor :status, :balance
+  
+    @@all = []
+  
+    def initialize(name, balance=1000, status="open")
+      @name = name
+      @status = status
+      @balance = balance
+      @@all << self
+    end
+  
+    def self.all
+      @@all
+    end
+  
+    def deposit(deposit)
+        self.balance += deposit
+    end
+  
+    def display_balance
+        "Your balance is $#{self.balance}."
+    end
+
+    def valid?
+        if self.status == "open" && self.balance > 0
+            true
+        else false
+        end
+    end
+
+    def close_account
+        self.status = "closed"
+    end
 
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,49 @@
+require 'pry'
+
 class Transfer
-  # your code here
+  attr_reader :sender, :receiver
+  attr_accessor :status, :amount
+
+  @@all = []
+
+  def initialize(sender, receiver, amount, status="pending")
+    @status = status
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def valid?
+    if sender.valid? && receiver.valid?
+      true
+    else false
+    end
+  end
+
+  def execute_transaction
+    if self.status == ("complete" || "rejected")
+      return "Transaction rejected. Please check your account balance."
+    elsif sender.balance < amount || !valid?
+      self.status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+    else
+      sender.balance -= amount
+      receiver.balance += amount
+      self.status = "complete"
+    end
+  end
+
+  def reverse_transfer
+    if self.status == "complete"
+      sender.balance += amount
+      receiver.balance -= amount
+      self.status = "reversed"
+    end
+  end
+
 end
